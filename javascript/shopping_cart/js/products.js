bf7c9c20785d6699b66db32134951f17248d1d15
azimhatami <azimhatami4@gmail.com>
2024-08-29 21:30:11 +0330

    Create products data

---
+++ javascript/shopping_cart/js/products.js
@@ -0,0 +1,51 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "queen panel bed",
+    price: 10.99,
+    imageUrl: "../images/1.png",
+  },
+  {
+    id: 2,
+    title: "saheb panel bed",
+    price: 14.99,
+    imageUrl: "../images/2.png",
+  },
+  {
+    id: 3,
+    title: "folani panel bed",
+    price: 7.99,
+    imageUrl: "../images/3.png",
+  },
+  {
+    id: 4,
+    title: "twin panel bed",
+    price: 11.99,
+    imageUrl: "../images/4.png",
+  },
+  {
+    id: 5,
+    title: "fridge",
+    price: 4.99,
+    imageUrl: "../images/5.png",
+  },
+  {
+    id: 6,
+    title: "Sara panel bed",
+    price: 23.99,
+    imageUrl: "../images/6.png",
+  },
+  {
+    id: 7,
+    title: "dresser",
+    price: 42.99,
+    imageUrl: "../images/7.png",
+  },
+  {
+    id: 8,
+    title: "couch",
+    price: 24.99,
+    imageUrl: "../images/8.png",
+  },
+];
+
