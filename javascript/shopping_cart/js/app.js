ab585ffdd95f3f79152cd5dded822dcc87499074
azimhatami <azimhatami4@gmail.com>
2024-08-29 21:33:59 +0330

    Get data and save in localstorage

--- javascript/shopping_cart/js/app.js
+++ javascript/shopping_cart/js/app.js
@@ -1,7 +1,57 @@
+import {productsData} from './products.js';
+
 const showBtn = document.querySelector('.fa-cart-shopping');
 const cartModal = document.querySelector('.modal-container');
 const backDrop = document.querySelector('.backdrop');
-const closeBtn = document.querySelector('.cart-item-confirm');
+const hideBtn = document.querySelector('.cart-item-confirm');
+const productsDOM = document.querySelector('.products');
+
+
+class Products {
+  getProduct() {
+    return productsData;
+  }
+}
+
+
+class UI {
+  displayProducts(products) {
+    let result = '';
+    products.forEach((item) => {
+      result += `<div class='product'>
+          <div class='img-container'>
+            <img src=${item.imageUrl} alt='product-1'>
+          </div>
+          <div class='description'>
+            <p class='description-title'>${item.title}</p>
+            <p class='description-price'>${item.price} $</p>
+          </div>
+          <button class='btn' data-id=${item.id}>Add to cart</button>
+        </div>`;
+
+      productsDOM.innerHTML = result;
+    });
+  }
+}
+
+
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('Loaded');
+  const products = new Products();
+  const productsData = products.getProduct();
+  const ui = new UI();
+  ui.displayProducts(productsData);
+  Storage.saveProducts(productsData);
+});
+
+
 
 
 function showModal() {
