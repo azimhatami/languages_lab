47182aca925aea2d06aecb77b710a40a168b8ba1
azimhatami <azimhatami4@gmail.com>
2024-09-18 13:50:20 +0330

    Save and delete note

--- javascript/note_app/main.js
+++ javascript/note_app/main.js
@@ -21,15 +21,32 @@ const notes = [
 
 class NotesAPI {
   static getAllNotes() {
-    const savedNotes = notes || [];
+    const savedNotes = JSON.parse(localStorage.getItem('notes-app')) || [];
     return savedNotes.sort((a, b) => {
       return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
     });
   }
-  saveNote() {
+  static saveNote(note) {
+    const notes = NotesAPI.getAllNotes();
+    const existedNote = notes.find((n) => n.id === note.id);
+    
+    if (existedNote) {
+      existedNote.title = note.title;
+      existedNote.body = note.body;
+      existedNote.updated = new Date().toISOString();
+    } else {
+      note.id = new Date().getTime();
+      note.updated = new Date().toISOString();
+      notes.push(note);
+    }
+
+    localStorage.setItem('notes-app', JSON.stringify(notes));
   }
-  deleteNote() {
+  static deleteNote(id) {
+    const notes = NotesAPI.getAllNotes();
+    const filteredNotes = notes.filter(n => n.id !== id)
+    localStorage.setItem('notes-app', JSON.stringify(filteredNotes));
   }
 }
 
-console.log(NotesAPI.getAllNotes());
+console.log(NotesAPI.deleteNote(3))
