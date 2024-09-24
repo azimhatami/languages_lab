510a204bc49f38521a2eba626aa22c28394681be
azimhatami <azimhatami4@gmail.com>
2024-09-24 14:48:17 +0330

    Babel & webpack tutorial

---
+++ javascript/babel/src/app.js
@@ -0,0 +1,13 @@
+const name = 'Azim';
+
+const welcomeMessage = () => 'Welcome thereeee!';
+
+class App {
+  console_message() {
+    console.log('hiiiii');
+  }
+}
+
+new App().console_message();
+
+console.log(welcomeMessage());
---
+++ javascript/babel/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: '/src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        }
+      }
+    ]
+  },
+}
