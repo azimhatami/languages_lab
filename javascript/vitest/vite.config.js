2d46f283e086170e08af45eb868f181b036657ff
Azim Hatami <azimhatami4@gmail.com>
2025-01-16 19:29:27 +0330

    Coverage Setup

---
+++ javascript/vitest/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: 'v8', // or 'v8'
+      reporter: ['text', 'html'],
+
+    },
+  },
+})
