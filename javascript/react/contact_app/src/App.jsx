6aa80b3c622e688ee8430b6e650b1c05a7d659e4
Azim Hatami <azimhatami.dev@gmail.com>
2025-02-07 19:26:57 +0330

    Config tailwind css and added Header component

--- javascript/react/contact_app/src/App.jsx
+++ javascript/react/contact_app/src/App.jsx
@@ -1,8 +1,11 @@
+import Header from './components/Header';
+
+
 function App() {
 
   return (
     <>
-      <h1>Contact App</h1>
+      <Header />
     </>
   )
 }
---
+++ javascript/react/contact_app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return(
+    <div className='!my-10 text-center'>
+      <h1 className='text-[#304ffe] text-4xl font-bold !mb-5'>Contact App</h1>
+      <p className='text-gray-500 font-bold'>
+        <a href='#' className='bg-blue-100 hover:bg-blue-500 hover:text-white !p-1.5 rounded-md transition-all delay-150 duration-300 ease-in-out'>Azim Hatami</a> | React js project
+      </p>
+    </div>
+  );
+}
+
+export default Header
