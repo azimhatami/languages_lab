cb2745b56c716247eec8db4792102aae622c5526
Azim Hatami <azimhatami.dev@gmail.com>
2025-02-25 19:57:23 +0330

    Learn different content types for web pages

--- javascript/nodejs/lesson03/index.js
+++ javascript/nodejs/lesson03/index.js
@@ -1,8 +1,19 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello NodeJs');
+  // response.writeHead(200, {'Content-Type': 'text/plain'});
+  // response.writeHead(200, {'Content-Type': 'text/html'});
+  // response.writeHead(200, {'Content-Type': 'application/json'});
+  // XML
+  response.writeHead(200, {'Content-Type': 'text/xml'});
+  // response.end('Hello NodeJs');
+  // response.end('<h1>Hello NodeJs</h1>');
+  // response.write(JSON.stringify({
+  //   name: 'NodeJS',
+  //   course: 'NodeJS And ExpressJS'
+  // }))
+  response.write('<xml><key>This is a key</key></xml>')
+  response.end();
 }).listen(4000, () => {
   console.log('server run on port 4000 : http://localhost:4000')
 })
