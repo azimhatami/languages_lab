ebee915a509d7ca81b78c3e3ee7f0d84b0d306b0
Azim Hatami <azimhatami.dev@gmail.com>
2025-02-25 13:27:16 +0330

    Learn ESM

---
+++ javascript/nodejs/lesson02/Esmodule.js
@@ -0,0 +1,17 @@
+// ESM
+
+const str = 'This is a string';
+export default str;
+
+function add(a, b) {
+  return a + b
+}
+
+function mulNums(a, b) {
+  return a * b
+}
+
+export {
+  add, 
+  mulNums
+}
--- javascript/nodejs/lesson02/index.js
+++ javascript/nodejs/lesson02/index.js
@@ -2,6 +2,8 @@
 // Es Module -> import, export, export default
 // Commonjs Module -> require, module.exports.foo, module.exports
 
-const { add, mulNums } = require('./Commonjs_module');
+// const { add, mulNums } = require('./Commonjs_module');
 
-console.log(add(2, 5), mulNums(2, 5));
+import str, {add, mulNums} from './Esmodule.js';
+
+console.log(add(2, 5), mulNums(2, 5), str);
