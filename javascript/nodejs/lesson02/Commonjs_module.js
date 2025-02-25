cd999b12a42a300b67a1069f848480868fd409b2
Azim Hatami <azimhatami.dev@gmail.com>
2025-02-25 11:52:44 +0330

    Learn: npm init & Esmodule & Commonjsmodule

---
+++ javascript/nodejs/lesson02/Commonjs_module.js
@@ -0,0 +1,18 @@
+// module.exports = 'String for test';  // export default 'string for test'
+
+function add(a, b) {
+  return a + b
+}
+
+function mulNums(a, b) {
+  return a * b
+}
+
+
+
+module.exports = {
+  add, 
+  mulNums,
+};
+
+// module.exports.str = 'This is a test';
---
+++ javascript/nodejs/lesson02/index.js
@@ -0,0 +1,7 @@
+// Modules
+// Es Module -> import, export, export default
+// Commonjs Module -> require, module.exports.foo, module.exports
+
+const { add, mulNums } = require('./Commonjs_module');
+
+console.log(add(2, 5), mulNums(2, 5));
