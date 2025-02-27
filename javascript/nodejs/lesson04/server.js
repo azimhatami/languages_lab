7219f7c73044d3dd3e006ee840fc2730021efae2
Azim Hatami <azimhatami.dev@gmail.com>
2025-02-27 20:26:49 +0330

    Refactor: clean up and organize project code

--- javascript/nodejs/lesson04/server.js
+++ javascript/nodejs/lesson04/server.js
@@ -3,16 +3,20 @@ const ProductsController = require('./controllers/product.controllers');
 const ErrorHandler = require('./controllers/errorHandler.controller');
 const PORT = 3000
 const server = http.createServer((req, res) => {
-  console.log(req.method)
-  if (req.url == '/api/products' && req.method == 'GET') {
+  const apiRoute = 'api';
+  const productsRoute = `${apiRoute}/products`;
+  const singleProductRoute = /\/api\/products\/[0-9]+/
+  const { url, method } = req;
+
+  if (url == productsRoute && method == 'GET') {
     ProductsController.get(req, res)
-  } else if (req.url.match(/\/api\/products\/[0-9]+/) && req.method == 'GET') {
+  } else if (url.match(singleProductRoute) && method == 'GET') {
     ProductsController.getById(req, res)
-  } else if (req.url == '/api/products' && req.method == 'POST') {
+  } else if (url == productsRoute && method == 'POST') {
     ProductsController.create(req, res)
-  } else if (req.url.match(/\/api\/products\/[0-9]+/) && req.method == 'PUT') {
+  } else if (url.match(singleProductRoute) && method == 'PUT') {
     ProductsController.update(req, res)
-  } else if (req.url.match(/\/api\/products\/[0-9]+/) && req.method == 'DELETE') {
+  } else if (url.match(singleProductRoute) && method == 'DELETE') {
     ProductsController.remove(req, res)
   } else {
     ErrorHandler.notFound(res)
