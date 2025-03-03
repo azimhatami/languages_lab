1536c78e1ec3eef7a7dbc114e0f9cc4be99c8c1d
Azim Hatami <azimhatami.dev@gmail.com>
2025-03-03 23:21:40 +0330

    Learn path module

---
+++ javascript/nodejs/path/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+// console.log('mongodb' + path.sep + 'data' + path.sep + 'users'); 
+// console.log('mongodb' + path.delimiter + 'data' + path.delimiter + 'users'); 
+// console.log(path.basename('../mongodb/server.js', '.js'))
+// console.log(path.dirname('mongodb/index.js'))
+// console.log(path.extname('index.html'))
+// console.log(path.extname('index.js'))
+// console.log(path.extname('main.py'))
+// console.log(path.extname('photo.png'))
+// console.log(path.join('/', 'mini_project', 'model', 'product.model.js'))
+// console.log(path.isAbsolute(path.join('/', 'mini_project', 'model', 'product.model.js')))
+// console.log(path.parse('/projects/mini_projects/controller/product.controller.js'))
+// console.log(path.normalize('//chapter2/lesson05/model//product.model.js'));
+// console.log(__dirname)
+console.log(__filename)
